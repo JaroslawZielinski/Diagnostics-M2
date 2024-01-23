5fb31d034974dfc079fe02bfee56f4aa21410030
Jarosław Zieliński <jaroslaw.zielinski@polcode.net>
2024-01-23 16:59:45 +0100

    Progress Bar Enahncement

--- Model/Config.php
+++ Model/Config.php
@@ -10,6 +10,9 @@ use Magento\Store\Model\ScopeInterface;
 class Config
 {
     public const PATH_SETTINGS_STORE_TELLER_ENABLE = 'jaroslawzielinski_diagnostics/settings/store_teller_enable';
+
+    public const PATH_PROGRESSBAR_FORMAT = 'jaroslawzielinski_diagnostics/progress_bar/format';
+
     /**
      * @var ScopeConfigInterface
      */
@@ -26,5 +29,12 @@ class Config
     {
         return $this->scopeConfig
             ->isSetFlag(self::PATH_SETTINGS_STORE_TELLER_ENABLE, ScopeInterface::SCOPE_STORE);
+    }
+
+    public function getProgressBarFormat(): ?string
+    {
+        $progressBarFormat = $this->scopeConfig
+            ->getValue(self::PATH_PROGRESSBAR_FORMAT);
+        return empty($progressBarFormat) ? null : (string)$progressBarFormat;
     }
 }
