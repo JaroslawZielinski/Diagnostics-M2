6ba4cb02bb12453f0be295c1a085ba49655e0f39
Jerry <jerry@livingproofcreative.com>
2025-09-24 10:35:41 +0200

    Add module.zip and SSL fixes

--- Helper/Data.php
+++ Helper/Data.php
@@ -8,20 +8,34 @@ use Magento\Framework\App\Helper\AbstractHelper;
 
 class Data extends AbstractHelper
 {
+    /**
+     */
     public static function dateDaysDiff(\DateTime $dateTime1, \DateTime $dateTime2): int
     {
         $diff = $dateTime1->diff($dateTime2);
         return (int)$diff->format('%r%a');
     }
 
-    public static function prefixStringArray(?array $input, string $prefix = ''): ?array
+    /**
+     */
+    public static function prefixStringArray(?array $input, string $prefix = ''): array
     {
         if (empty($input)) {
-            return null;
+            return [];
         }
         return array_values(array_map(function ($value) use ($prefix) {
             $domain = reset($value);
             return $prefix . $domain;
         }, $input));
+    }
+
+    /**
+     */
+    public static function getSerializedArrayOrEmpty(?string $serializedArray = ''): string
+    {
+        if (empty($serializedArray) || '[]' === $serializedArray) {
+            return '{}';
+        }
+        return $serializedArray;
     }
 }
