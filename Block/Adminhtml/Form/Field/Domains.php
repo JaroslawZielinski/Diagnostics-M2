998222356d64d9b9846d1a40628d51119fd24be3
Jarosław Zieliński <jarek.zielinski82@gmail.com>
2026-02-09 11:55:16 +0100

    Abstract Grid Admin Enhancement

--- Block/Adminhtml/Form/Field/Domains.php
+++ Block/Adminhtml/Form/Field/Domains.php
@@ -5,8 +5,9 @@ declare(strict_types=1);
 namespace JaroslawZielinski\Diagnostics\Block\Adminhtml\Form\Field;
 
 use Magento\Config\Block\System\Config\Form\Field\FieldArray\AbstractFieldArray;
+use JaroslawZielinski\Diagnostics\Block\Adminhtml\AbstractDraggableFieldArray;
 
-class Domains extends AbstractFieldArray
+class Domains extends AbstractDraggableFieldArray
 {
     public const NAME = 'domain';
 
@@ -21,5 +22,12 @@ class Domains extends AbstractFieldArray
         ]);
         $this->_addAfter = true;
         $this->_addButtonLabel = __('Add domain');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function getRowsHtmlId(): string {
+        return 'row_jaroslawzielinski_diagnostics_ssl_certificates_check_staging_domains';
     }
 }
